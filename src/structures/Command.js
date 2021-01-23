748427e7055de22b12983f87c86bca79ee7edb66
düsk <68671155+devduskk@users.noreply.github.com>
2021-01-23 19:16:06 -0300

    IDK

---
+++ src/structures/Command.js
@@ -0,0 +1,25 @@
+const { Permissions } = require("discord.js");
+
+module.exports = class Command {
+
+    constructor(client, name, options = {}) {
+        this.client = client;
+        this.name = options.name || name;
+        this.aliases = options.aliases || [];
+        this.enabled = options.enabled || false;
+        this.description = options.description || "Unspecified description.";
+        this.category = options.category || "Miscellaneous";
+        this.usage = `${this.client.prefix}${this.name}: ${options.usage || ''}`.trim();
+        this.botPermissions = new Array();
+        this.memberPermissions = new Array();
+        this.guildOnly = options.guildOnly || false;
+        this.ownerOnly = options.ownerOnly || false;
+        this.nsfw = options.nsfw || false;
+        this.args = options.args || false;
+        this.cooldown = options.cooldown || false;
+    }
+
+    async run(message, args) {
+        throw new Error(`The command ${this.name} didn't specify a boot method.`)
+    }
+}

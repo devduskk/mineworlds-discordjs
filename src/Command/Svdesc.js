11d0261fe8915e1c723c66c8e845ba902674b6c2
dusk <68671155+devduskk@users.noreply.github.com>
2021-03-15 20:13:29 -0300

    Update - Svdesc.js

--- src/Command/Svdesc.js
+++ src/Command/Svdesc.js
@@ -1,6 +1,6 @@
-const Command = require('../structures/Command');
-const { MessageEmbed } = require('discord.js');
-const db = require('quick.db');
+const Command = require('../structures/Command'),
+    { MessageEmbed } = require('discord.js'),
+    settings = require('quick.db');
 
 module.exports = class extends Command {
 
@@ -8,28 +8,22 @@ module.exports = class extends Command {
         super(...args, {
             aliases: ['svdesc'],
             enabled: true,
-            description: 'Command used to enter the server description.',
-            category: 'Utility',
-            usage: '\`svdesc\`',
             guildOnly: false,
             ownerOnly: false,
-            nsfw: false,
-            args: false,
-            cooldown: 5000
         })
     }
 
     async run(message, args) {
         message.delete();
 
-        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("OOPS! It looks like you don't have enough permission.").then(async (msg) => {
+        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("Eita! Parece que você não tem permissão suficiente.").then(async (msg) => {
             await msg.delete({ timeout: 5 * 1000 });
         });
 
-        var serverDescription = args.join(" ");
-        if(serverDescription) return message.channel.send(`Success! The server description has been set correctly.`).then(async (msg) => {
+        var messager = args.join(" ");
+        message.channel.send(`Sucesso! A descrição do servidor foi setada corretamente.`).then(async (msg) => {
             await msg.delete({ timeout: 5 * 1000 });
         });
-        db.set(`svdesc_${message.guild.id}`, serverDescription);
+        settings.set(`svdesc_${message.guild.id}`, messager);
     }
 }
